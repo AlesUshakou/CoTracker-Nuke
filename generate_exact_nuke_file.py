58e7e8d82b9ef503f6425316c56c4ca2468aa145
Aleš Ushakou <AlesUshakou@users.noreply.github.com>
2025-11-12 14:44:56 +0300

    fixed correct keyframe offset and simplified Tracker4 generation

--- generate_exact_nuke_file.py
+++ generate_exact_nuke_file.py
@@ -104,70 +104,30 @@ def generate_nuke_file(csv_path, output_path=None, image_height=1080, min_confid
         x_curve = " ".join(x_values)
         y_curve = " ".join(y_values)
         
+        frame_start = reference_frame
+        name = f'track {i+1}'
+        x_str = x_curve
+        y_str = y_curve
+        trs_val = "1 0 0"
+
+
         # Create track line (based on analysis of working .nk file)
-        track_line = f' {{ {{curve K 1}} "track {i+1}" {{curve {x_curve}}} {{curve {y_curve}}} {{curve K 0}} {{curve K 0}} 1 0 0 {{curve 0}} 1 0 -32 -32 32 32 -22 -22 22 22 {{}} {{}}  {{}}  {{}}  {{}}  {{}}  {{}}  {{}}  {{}}  {{}}  {{}}   }}'
+        track_line =  ''' { {curve K x%s 1} "%s" {curve x%s %s} {curve x%s %s} 
+                            {curve K x%s 0} {curve K x%s 0} %s {curve x%s 0} 
+                            1 0 -15 -15 15 15 -10 -10 10 10 {} {}  {}  {}  {}  {}  {}  {}  {}  {}  {}   }''' % (    frame_start, name,
+                                                                                                                    frame_start, x_str,
+                                                                                                                    frame_start, y_str,
+                                                                                                                    frame_start,
+                                                                                                                    frame_start,
+                                                                                                                    trs_val,
+                                                                                                                    frame_start
+                                                                                                                    )
         track_data_lines.append(track_line)
+
+
     
     # Generate complete .nk file content
-    nk_content = f'''#! C:/Program Files/Nuke16.0v5/nuke-16.0.5.dll -nx
-version 16.0 v5
-define_window_layout_xml {{<?xml version="1.0" encoding="UTF-8"?>
-<layout version="1.0">
-    <window x="-2556" y="-5" w="5114" h="1361" screen="1">
-        <splitter orientation="1">
-            <split size="40"/>
-            <dock id="" hideTitles="1" activePageId="Toolbar.1">
-                <page id="Toolbar.1"/>
-            </dock>
-            <split size="2512" stretch="1"/>
-            <splitter orientation="1">
-                <split size="2512"/>
-                <dock id="" activePageId="Viewer.1">
-                    <page id="Viewer.1"/>
-                </dock>
-            </splitter>
-            <split size="2554"/>
-            <splitter orientation="2">
-                <split size="756"/>
-                <splitter orientation="1">
-                    <split size="603"/>
-                    <dock id="" activePageId="Properties.1">
-                        <page id="Properties.1"/>
-                    </dock>
-                    <split size="1947"/>
-                    <dock id="" activePageId="DAG.1" focus="true">
-                        <page id="DAG.1"/>
-                    </dock>
-                </splitter>
-                <split size="563"/>
-                <dock id="" activePageId="uk.co.thefoundry.scripteditor.1">
-                    <page id="DopeSheet.1"/>
-                    <page id="Curve Editor.1"/>
-                    <page id="uk.co.thefoundry.backgroundrenderview.1"/>
-                    <page id="uk.co.thefoundry.scripteditor.1"/>
-                </dock>
-            </splitter>
-        </splitter>
-    </window>
-</layout>
-}}
-Root {{
-inputs 0
-name {output_path}
-frame 0
-format "2048 1556 0 0 2048 1556 1 2K_Super_35(full-ap)"
-proxy_type scale
-proxy_format "1024 778 0 0 1024 778 1 1K_Super_35(full-ap)"
-colorManagement Nuke
-workingSpaceLUT linear
-monitorLut sRGB
-int8Lut sRGB
-int16Lut sRGB
-logLut Cineon
-floatLut linear
-}}
-Tracker4 {{
-inputs 0
+    nk_content = f'''Tracker4 {{
 tracks {{ {{ 1 31 {num_tracks} }} 
 {{ {{ 5 1 20 enable e 1 }} 
 {{ 3 1 75 name name 1 }} 
@@ -207,9 +167,6 @@ tracks {{ {{ 1 31 {num_tracks} }}
 }}
 reference_frame {reference_frame}
 name CoTracker_Generated_{num_tracks}pts
-selected true
-xpos 0
-ypos 0
 }}
 
 '''
@@ -225,22 +182,21 @@ ypos 0
     
     return output_path
 
+
 if __name__ == "__main__":
     import sys
-    
-    # Check if called with command line arguments (from nuke_exporter.py)
+    # CLI usage:
+    # python generate_exact_nuke_file.py csv_path output_path frame_offset reference_frame_video image_height
     if len(sys.argv) >= 6:
         csv_path = sys.argv[1]
         output_path = sys.argv[2]
         frame_offset = int(sys.argv[3])
-        reference_frame_video = int(sys.argv[4])  # 0-based video frame index
+        reference_frame_video = int(sys.argv[4])
         image_height = int(sys.argv[5])
-        
-        # Convert 0-based video reference frame to user's actual reference frame number
+
         reference_frame = reference_frame_video + frame_offset
-        
-        # Generate .nk file with provided parameters
-        output_file = generate_nuke_file(
+
+        out = generate_nuke_file(
             csv_path=csv_path,
             output_path=output_path,
             image_height=image_height,
@@ -248,24 +204,7 @@ if __name__ == "__main__":
             frame_offset=frame_offset,
             reference_frame=reference_frame
         )
-        
-        # Print the absolute path (nuke_exporter.py expects this on the last line)
-        print(os.path.abspath(output_file))
-        
+        print(os.path.abspath(out))
     else:
-        # Configuration for standalone use
-        csv_path = "Z:/Dev/Cotracker/temp/full_coords_20250928_171202.csv"
-        image_height = 1080
-        min_confidence = 0.5
-        
-        # Generate .nk file
-        output_file = generate_nuke_file(
-            csv_path=csv_path,
-            image_height=image_height,
-            min_confidence=min_confidence
-        )
-        
-        print(f"TO USE:")
-        print(f"   1. Open Nuke")
-        print(f"   2. File > Open > {output_file}")
-        print(f"   3. Tracker4 node will be loaded with all tracking data")
+        print("Usage: python generate_exact_nuke_file.py <csv_path> <output_path> <frame_offset> <reference_frame_video> <image_height>")
+        print("Example: python generate_exact_nuke_file.py coords.csv out.nk 0 1001 1080")
